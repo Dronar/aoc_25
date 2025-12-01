10382cfd601a821c4904e823eb9dded61e3645cf
Magnus Jonsson <magnus@dronar.se>
2025-12-01 10:02:26 +0100

    Day 1, part I

---
+++ 01/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	start_time := time.Now()
+	numbers := getNumbers()
+
+	current := 50
+	counter := 0
+
+	for _, v := range numbers {
+		current += v % 100
+		if current > 99 {
+			current = current % 100
+		}
+		if current < 0 {
+			current = 100 + current
+		}
+
+		if current == 0 {
+			counter++
+		}
+	}
+
+	fmt.Println("Answer:", counter)
+	fmt.Println("Duration:", time.Since(start_time))
+}
+
+func getNumbers() []int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	var numbers = make([]int, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number_string := scanner.Text()
+		number, _ := strconv.Atoi(number_string[1:])
+
+		if number_string[0] == 'R' {
+			numbers = append(numbers, number)
+		} else {
+			numbers = append(numbers, 0-number)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	return numbers
+}
