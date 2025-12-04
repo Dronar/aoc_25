58a10becc37ff434edb93ce34bf31759369628e3
Magnus Jonsson <magnus@dronar.se>
2025-12-04 08:57:21 +0100

    Day 4, part II

--- 04/main.go
+++ 04/main.go
@@ -12,6 +12,13 @@ func main() {
 
 	grid := getGrid()
 
+	moveable_rolls := step2(grid)
+
+	fmt.Println("Moveable rolls:", moveable_rolls)
+	fmt.Println("Duration:", time.Since(start_time))
+}
+
+func step1(grid [][]byte) int {
 	moveable_rolls := 0
 
 	for y, row := range grid {
@@ -62,8 +69,71 @@ func main() {
 		}
 	}
 
-	fmt.Println("Moveable rolls:", moveable_rolls)
-	fmt.Println("Duration:", time.Since(start_time))
+	return moveable_rolls
+}
+
+func step2(grid [][]byte) int {
+	moveable_rolls := 0
+
+	moved_this_round := true
+
+	for moved_this_round {
+		var rolls_to_remove [][2]int
+		for y, row := range grid {
+			for x, value := range row {
+				if value == byte('.') {
+					continue
+				}
+
+				adjacent_rolls := 0
+
+				// Check above
+				if y > 0 {
+					if x > 0 && grid[y-1][x-1] == byte('@') {
+						adjacent_rolls++
+					}
+					if grid[y-1][x] == byte('@') {
+						adjacent_rolls++
+					}
+					if x < len(row)-1 && grid[y-1][x+1] == byte('@') {
+						adjacent_rolls++
+					}
+				}
+
+				// Check same line
+				if x > 0 && grid[y][x-1] == byte('@') {
+					adjacent_rolls++
+				}
+				if x < len(row)-1 && grid[y][x+1] == byte('@') {
+					adjacent_rolls++
+				}
+
+				// Check below
+				if y < len(grid)-1 {
+					if x > 0 && grid[y+1][x-1] == byte('@') {
+						adjacent_rolls++
+					}
+					if grid[y+1][x] == byte('@') {
+						adjacent_rolls++
+					}
+					if x < len(row)-1 && grid[y+1][x+1] == byte('@') {
+						adjacent_rolls++
+					}
+				}
+
+				if adjacent_rolls < 4 {
+					rolls_to_remove = append(rolls_to_remove, [2]int{y, x})
+				}
+			}
+		}
+		for _, coordinates := range rolls_to_remove {
+			grid[coordinates[0]][coordinates[1]] = byte('.')
+		}
+		moveable_rolls += len(rolls_to_remove)
+		moved_this_round = len(rolls_to_remove) > 0
+	}
+
+	return moveable_rolls
 }
 
 func getGrid() [][]byte {
