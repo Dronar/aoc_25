f534abbff3dc3ece2dc08bd697c7f852c28a29c2
Magnus Jonsson <magnus@dronar.se>
2025-12-05 15:38:57 +0100

    Day 2, part II

--- 05/main.go
+++ 05/main.go
@@ -12,7 +12,16 @@ import (
 func main() {
 	start_time := time.Now()
 
-	fresh_ranges, ingredients := getData()
+	fresh_ranges, _ := getData()
+
+	//fresh_count := step1(fresh_ranges, ingredients)
+	fresh_count := step2(fresh_ranges)
+
+	fmt.Println("Fresh ingredients:", fresh_count)
+	fmt.Println("Duration:", time.Since(start_time))
+}
+
+func step1(fresh_ranges [][2]int, ingredients []int) int {
 	fresh_count := 0
 
 	for _, item := range ingredients {
@@ -24,8 +33,77 @@ func main() {
 		}
 	}
 
-	fmt.Println("Fresh ingredients:", fresh_count)
-	fmt.Println("Duration:", time.Since(start_time))
+	return fresh_count
+}
+
+func step2(fresh_ranges [][2]int) int {
+	var union_ranges [][2]int
+	for _, fresh_range := range fresh_ranges {
+		should_insert := true
+		var ranges_to_merge []int
+
+		//fmt.Printf("Range: %d\n", fresh_range)
+		for i, unique_range := range union_ranges {
+			// If new is inside existing range
+			if fresh_range[0] >= unique_range[0] && fresh_range[1] <= unique_range[1] {
+				ranges_to_merge = append(ranges_to_merge, i)
+				should_insert = false
+				continue
+			}
+			// If existing range is inside new
+			if fresh_range[0] <= unique_range[0] && fresh_range[1] >= unique_range[1] {
+				ranges_to_merge = append(ranges_to_merge, i)
+				should_insert = false
+				continue
+			}
+			// If new is partly inside range (higher)
+			if fresh_range[0] >= unique_range[0] && fresh_range[0] <= unique_range[1] {
+				ranges_to_merge = append(ranges_to_merge, i)
+				should_insert = false
+				continue
+			}
+			// If new is partly inside range (lower)
+			if fresh_range[1] >= unique_range[0] && fresh_range[1] <= unique_range[1] {
+				ranges_to_merge = append(ranges_to_merge, i)
+				should_insert = false
+				continue
+			}
+		}
+
+		if len(ranges_to_merge) > 0 {
+			// Dummy copy to iterate over
+			range_copy := append([][2]int(nil), union_ranges...)
+
+			min := fresh_range[0]
+			max := fresh_range[1]
+
+			for _, r := range ranges_to_merge {
+				if range_copy[r][0] < min {
+					min = range_copy[r][0]
+				}
+				if range_copy[r][1] > max {
+					max = range_copy[r][1]
+				}
+
+				// Delete range in original array
+				union_ranges[r] = union_ranges[len(union_ranges)-1]
+				union_ranges = union_ranges[:len(union_ranges)-1]
+			}
+
+			union_ranges = append(union_ranges, [2]int{min, max})
+		}
+		if should_insert {
+			union_ranges = append(union_ranges, fresh_range)
+		}
+	}
+
+	ingredient_count := 0
+
+	for _, unique_range := range union_ranges {
+		ingredient_count += unique_range[1] - unique_range[0] + 1
+	}
+
+	return ingredient_count
 }
 
 func getData() ([][2]int, []int) {
