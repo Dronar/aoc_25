921565d7351e66d94b5e6bb0dc0ac569ea9dbce6
Magnus Jonsson <magnus@dronar.se>
2025-12-08 10:24:42 +0100

    Day 7, part II

--- 07/main.go
+++ 07/main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	start_time := time.Now()
-	hits := calculate()
+	hits := step2()
 	fmt.Println("Hits:", hits)
 	fmt.Println("Duration:", time.Since(start_time))
 }
 
-func calculate() int {
+func step1() int {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -55,5 +55,59 @@ func calculate() int {
 		i++
 	}
 
+	return counter
+}
+
+func step2() int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var grid [][]int
+	i := 0
+
+	for scanner.Scan() {
+		line := []byte(scanner.Text())
+		var row []int
+
+		j := 0
+		for j < len(line) {
+			value := line[j]
+			if value == byte('.') {
+				if i > 0 && grid[i-1][j] > 0 {
+					row = append(row, grid[i-1][j])
+				} else {
+					row = append(row, 0)
+				}
+			}
+			if value == byte('S') {
+				row = append(row, 1)
+			}
+			if value == byte('^') {
+				beams := grid[i-1][j]
+				if beams > 0 {
+					row[j-1] += beams
+					row = append(row, 0)
+					row = append(row, beams+grid[i-1][j+1])
+					j++
+				} else {
+					row = append(row, 0)
+				}
+			}
+
+			j++
+		}
+
+		grid = append(grid, row)
+		i++
+	}
+
+	counter := 0
+	for _, beams := range grid[len(grid)-1] {
+		counter += beams
+	}
 	return counter
 }
