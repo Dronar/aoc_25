da94e1f6043c2c87f245897841fe25c7591a59d1
Magnus Jonsson <magnus@dronar.se>
2025-12-11 11:23:08 +0100

    Day 11, part II

--- 11/main.go
+++ 11/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -19,7 +20,7 @@ func NewGraph() *Graph {
 	return &Graph{vertices: make(map[string][]string)}
 }
 
-func (g *Graph) DFS(node string, target string, visited map[string]bool, sum *int) {
+func (g *Graph) DFS_part1(node string, target string, visited map[string]bool, sum *int) {
 	if node == target {
 		*sum++
 		return
@@ -28,11 +29,44 @@ func (g *Graph) DFS(node string, target string, visited map[string]bool, sum *in
 
 	for _, w := range g.vertices[node] {
 		if !visited[w] {
-			g.DFS(w, target, visited, sum)
+			g.DFS_part1(w, target, visited, sum)
 		}
 	}
 
 	visited[node] = false
+}
+
+func (g *Graph) DFS_part2(node string, target string, fft bool, dac bool, visited map[string]int) int {
+	cache_key := node + strconv.FormatBool(fft) + strconv.FormatBool(dac)
+	value, ok := visited[cache_key]
+	if ok {
+		return value
+	}
+	if node == target {
+		if fft && dac {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	if node == "fft" {
+		fft = true
+	}
+	if node == "dac" {
+		dac = true
+	}
+
+	paths := 0
+
+	for _, neighbor := range g.vertices[node] {
+
+		paths += g.DFS_part2(neighbor, target, fft, dac, visited)
+	}
+
+	visited[cache_key] = paths
+
+	return paths
 }
 
 func main() {
@@ -42,8 +76,11 @@ func main() {
 
 	valid_paths := 0
 
-	visited := make(map[string]bool)
-	connections.DFS("you", "out", visited, &valid_paths)
+	//visited1 := make(map[string]bool)
+	//connections.DFS_part1("svr", "out", visited1, &valid_paths)
+
+	visited2 := make(map[string]int)
+	valid_paths = connections.DFS_part2("svr", "out", false, false, visited2)
 
 	fmt.Println("Paths:", valid_paths)
 	fmt.Println("Duration:", time.Since(start_time))
